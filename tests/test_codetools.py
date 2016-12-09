45cc56d1709197f636d5e1b47cecf9916e964a69
adam <athornton@gmail.com>
2016-12-09 12:45:03 -0700

    Linting the tests so they are happy under Python 2 and 3.

--- tests/test_codetools.py
+++ tests/test_codetools.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+"""Test temporary directory context manager"""
 import os
 import codekit.codetools as codetools
 
 
 def test_tempdir():
+    """Test temporary directory context manager"""
     with codetools.TempDir() as temp_dir:
         assert os.path.exists(temp_dir)
     assert os.path.exists(temp_dir) is False
