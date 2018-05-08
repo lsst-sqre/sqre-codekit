822f9c48c9a36d2cfbb3fa0c62c940a77f104783
Joshua Hoblitt <josh@hoblitt.com>
2018-05-08 07:20:02 -0700

    add pygithub.find_tag_by_name()

--- codekit/pygithub.py
+++ codekit/pygithub.py
@@ -4,9 +4,10 @@ codetools.
 """
 
 import logging
+import github
+import codekit.codetools as codetools
 from public import public
 from github import Github
-import codekit.codetools as codetools
 
 logging.basicConfig()
 logger = logging.getLogger('codekit')
@@ -33,3 +34,30 @@ def login_github(token_path=None, token=None):
 
     token = codetools.github_token(token_path=token_path, token=token)
     return Github(token)
+
+
+@public
+def find_tag_by_name(repo, tag_name):
+    """Find tag by name in a github Repository
+
+    Parameters
+    ----------
+    repo: :class:`github.Repository` instance
+
+    tag_name: str
+        Short name of tag (not a fully qualified ref).
+
+    Returns
+    -------
+    gh : :class:`github.GitRef` instance or `None`
+    """
+    tagfmt = 'tags/{ref}'.format(ref=tag_name)
+
+    try:
+        ref = repo.get_git_ref(tagfmt)
+        if ref and ref.ref:
+            return ref
+    except github.UnknownObjectException:
+        pass
+
+    return None
