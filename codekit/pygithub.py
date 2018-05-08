7af2c0158e5a4a7cf8e371f1e0f9cf5c4d2e663a
Joshua Hoblitt <josh@hoblitt.com>
2018-05-08 08:33:56 -0700

    add safe param to pygithub.find_tag_by_name()
    safe=True (default) returns None instead of an exception upon failure.

--- codekit/pygithub.py
+++ codekit/pygithub.py
@@ -37,7 +37,7 @@ def login_github(token_path=None, token=None):
 
 
 @public
-def find_tag_by_name(repo, tag_name):
+def find_tag_by_name(repo, tag_name, safe=True):
     """Find tag by name in a github Repository
 
     Parameters
@@ -47,9 +47,18 @@ def find_tag_by_name(repo, tag_name):
     tag_name: str
         Short name of tag (not a fully qualified ref).
 
+    safe: bool, optional
+        Defaults to `True`. When `True`, `None` is returned on failure. When
+        `False`, an exception will be raised upon failure.
+
     Returns
     -------
     gh : :class:`github.GitRef` instance or `None`
+
+    Raises
+    ------
+    github.UnknownObjectException
+        If git tag name does not exist in repo.
     """
     tagfmt = 'tags/{ref}'.format(ref=tag_name)
 
@@ -58,6 +67,7 @@ def find_tag_by_name(repo, tag_name):
         if ref and ref.ref:
             return ref
     except github.UnknownObjectException:
-        pass
+        if not safe:
+            raise
 
     return None
